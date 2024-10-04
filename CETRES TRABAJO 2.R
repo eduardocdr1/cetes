2bcce8411a3629fa0227ff2b9a5fe7e5903dae61
eduardocdr1 <2211334d@umich.mx>
2024-10-04 13:23:39 -0600

    Add files via upload

---
+++ CETRES TRABAJO 2.R
@@ -0,0 +1,10 @@
+#Se presentan las funciones de interes simpĺe para CETES
+#Autor: Juan Eduardo Calderon del Rio
+#V.1.0: 3 de octubre del 2024
+
+#Valor futuro con CETES:
+
+valorFinalSimple=function(VA,r,t){
+  VF=VA*(1+(r*t))
+    
+  return(VF) }
